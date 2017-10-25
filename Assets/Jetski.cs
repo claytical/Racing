6f252f1586d7dbc40b914fed6de7e5875dda292c
Clay Ewing <c.ewing@miami.edu>
2017-10-25 18:11:58 -0400

    Initial commit

---
+++ Assets/Jetski.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jetski : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.LeftArrow)) {
+			
+			transform.Rotate (0, 0, -.01f);
+		}
+		if (Input.GetKey(KeyCode.RightArrow)) {
+			transform.Rotate (0, 0, .01f);
+		}
+
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			//model is rotated 90 degrees, otherwise we'd use transform.forward
+			transform.position += transform.up * .1f;
+		}
+	}
+}
